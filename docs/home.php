bfb0b840a3b98e51a067651f99433056e83451e9
Jason <js21kim@gmail.com>
2017-04-12 10:02:28 +0000

    home.php5

--- docs/home.php
+++ docs/home.php
@@ -12,28 +12,28 @@ include ( "./inc/connect.inc.php" );
     </head>
 
     <body>
-//        <?php include < "./headerMenu.php" ); ?>
+        <?php include "./headerMenu.php" ); ?>
 
-//        <div id="inner">
-//            <div id="sidebar">
-//                <?php
-//                    if (isset($_SESSION['u_id'])){
-//                        echo "HI";
-//                    }
-//                ?>
-//            </div>
+        <div id="inner">
+            <div id="sidebar">
+                <?php
+                    if (isset($_SESSION['u_id'])){
+                        echo "HI";
+                    }
+                ?>
+            </div>
 
-//            <div id="content">
-//                <?php
-//                    if (isset($_SESSION['u_id'])) {
-//                        echo "<p>Logged in as ". $_SESSION['u_id'] . "</p>";
-//                    }
+            <div id="content">
+                <?php
+                    if (isset($_SESSION['u_id'])) {
+                        echo "<p>Logged in as ". $_SESSION['u_id'] . "</p>";
+                    }
 
-//                    echo "<a href='prev_index.php'> Calendar </a>";
-//                ?>
-//            </div>
-//        </div>
+                    echo "<a href='prev_index.php'> Calendar </a>";
+                ?>
+            </div>
+        </div>
 
-//        <script src="./js/script1.js"></script>
+        <script src="./js/script1.js"></script>
     </body>
 </html>
