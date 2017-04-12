bcc216bff0cbd4e9f63d18178d0fea1d73923c43
Song,eunchul <sech9446@naver.com>
2017-04-12 12:30:56 +0900

    title update

--- docs/index.php
+++ docs/index.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="calendar.css"></link>
     <link rel="stylesheet" href="scheduler.css"></link>
+    <title> UniScheduler </title>
     </head>
     <body>
     <script src="calendar.js"></script>
