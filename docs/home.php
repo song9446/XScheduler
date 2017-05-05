4eac7c016d18586822ace2ada999849b3497283a
dongju <dju1006@unist.ac.kr>
2017-05-05 07:08:52 +0000

    dj

--- docs/home.php
+++ docs/home.php
@@ -30,9 +30,9 @@ include ( "./inc/connect.inc.php" );
                     echo "<a href='scheduler/index.php' font-size: 20px> Calendar <-- Song's Beautiful Calendar\n </a>";
                 ?>
                 <div id="description">
-                  <h1>UniScheduler</h1>
-                  This is Database lecture project.<br/>
-                  Contributor : Jason Kim, Eunchul Song, Dongju Shin<br/>
+                  <font size='23'>UniScheduler</font>
+                  <font size='18'>This is Database lecture project.<br/>
+                  Contributor : Jason Kim, Eunchul Song, Dongju Shin</font>
                 </div>
             </div>
         </div>
