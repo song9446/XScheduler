9adfc81102415982c94a3a82539edc5e45d79e0c
dongju <dju1006@unist.ac.kr>
2017-05-05 04:48:28 +0000

    dj

--- docs/headerMenu.php
+++ docs/headerMenu.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
     <div class="headerMenu">
-        <button type="button" id="sidebar_button" onclick="sidebar_toggle('sidebar_button')" value="0">SBB</button>
+        <!-- <button type="button" id="sidebar_button" onclick="sidebar_toggle('sidebar_button')" value="0">SBB</button> -->
         <div class="logo">
             <a href="home.php" class="logo">
                 <img src="./img/logo.png" width="42" height="42"/>
             </a>
         </div>
-        <div id="topMenu">
+        <nav id="topMenu">
           <ul>
               <li>
                 <a class="menuLink" href="#">Personal schedule</a>
@@ -15,7 +15,7 @@
                 <a class="menuLink" href="#">Group schedule</a>
               </li>
           </ul>
-        </div>
+        </nav>
             <?php
                 if (isset($_SESSION['u_id'])){
                     echo "<a href='logout.php' class='logout_button'>Logout</a>";
