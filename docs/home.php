45b6cca1614cbf847c32bcf56102fda1fc18dd69
Jason <js21kim@gmail.com>
2017-05-05 02:56:29 +0000

    friend link

--- docs/home.php
+++ docs/home.php
@@ -17,7 +17,7 @@ include ( "./inc/connect.inc.php" );
             <div id="sidebar">
                 <?php
                     if (isset($_SESSION['u_id'])){
-                        echo "HI";
+                        echo "<a href='friend/index.php' font-size: 20px> Friend </a>";
                     }
                 ?>
             </div>
@@ -27,14 +27,7 @@ include ( "./inc/connect.inc.php" );
                     if (isset($_SESSION['u_id'])) {
                         echo "<p>Logged in as ". $_SESSION['u_id'] . "</p>";
                     }
-<<<<<<< HEAD
-                    echo "<a href='prev_index.php' font-size: 20px> Calendar <-- Song's Beautiful Calendar\n </a>";
-                    echo "<a >Login is currently NOT working because it is not connected to our database.</a>";
-                    echo "<a >Previous 'index.php' has been moved to 'prev_index.php'.\n</a>";
-=======
                     echo "<a href='scheduler/index.php' font-size: 20px> Calendar <-- Song's Beautiful Calendar\n </a>";
-                    echo "<a >Login is currently NOT working because it is not connected to our database.</a>";
->>>>>>> 15f61c3456fc86acec784261f59e8235c2e49957
                 ?>
             </div>
         </div>
