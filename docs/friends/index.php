dc98d51e61ff15b21c435f9d55a2784eeb9e50a9
Jason <js21kim@gmail.com>
2017-05-05 02:53:39 +0000

    friends/index.php

---
+++ docs/friends/index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include ( "../inc/connect.inc.php" );
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>UniScheduler</title>
+        <link rel="stylesheet" type="text/css" href="../css/style.css" />
+    </head>
+
+    <body>
+
+        <?php
+            echo "!!";
+        ?>
+
+    </body>
+</html>
+
