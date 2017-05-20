9625a125ffa199e437d50703d2458c8df9175671
Jason <js21kim@gmail.com>
2017-05-20 02:03:45 +0000

    FAR12

--- docs/friends/add_friend.php
+++ docs/friends/add_friend.php
@@ -3,10 +3,10 @@ session_start();
 include ( "../inc/connect.inc.php" );
 
   
-if ( isset($_SESSION[`u_id`]) )
+if ( isset($_SESSION['u_id']) )
 {
-    if( isset($_GET[`add_friend_u_id`]) ){
-        $uif=$_SESSION[`u_id`]; // u_id_from
+    if( isset($_GET['add_friend_u_id']) ){
+        $uif=$_SESSION['u_id']; // u_id_from
         $uit=$_GET[`add_friend_u_id`]; // u_id_to
 
         $query = "INSERT INTO friend_request WHERE (u_id_from, u_id_to) VALUES ('$uif', '$uit')";
