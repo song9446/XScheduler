71dd61c01e3fc2382915faf08300f997d2b6e941
dongju <dju1006@unist.ac.kr>
2017-05-20 06:10:13 +0000

    dj

--- docs/group_sche_create.php
+++ docs/group_sche_create.php
@@ -14,13 +14,13 @@
         <input type="text" id="group_name" placeholder="Enter group name">
       </div>
       <div class="friends-select">
-        <select name='select' size='5' multiple>
+        <select name='select' size='100' multiple>
           <option value="1">Jason</option>
           <option value="2">Eunchul</option>
           <option value="3">Dongju</option>
         </select>
-        <div id="selected"> Should be implemented...
-        </div>
+      </div>
+      <div class="selected"> Should be implemented...
       </div>
       <input type="submit" id="btn-submit" value="Submit">
     </form>
