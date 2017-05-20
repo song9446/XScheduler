eefc5030d8e904c3c60ef21560448d9bedae885e
Jason <js21kim@gmail.com>
2017-05-20 05:25:33 +0000

    delete friend

---
+++ docs/friends/delete_friend.php
@@ -0,0 +1,26 @@
+ession_start();
+include ( "../inc/connect.inc.php" );
+
+
+if ( isset($_SESSION['u_id']) )
+{
+     if( isset($_GET['delete_friend_u_id']) ){
+         $curr_u_id=$_SESSION['u_id']; // current u_id
+         $delete_u_id=$_GET['delete_friend_u_id']; // target id of delete friend
+
+         $query = "DELETE FROM friend WHERE u_id = '$curr_u_id' AND f_u_id = '$delete_u_id'";
+         $result = mysqli_query($conn, $query);
+
+         if ($result) {
+            echo "Successfully deleted   " . $delete_u_id . " from friend list.";
+            echo "<meta http-equiv='refresh' content='0;url=home.php'>";
+         }
+         else {
+             echo "Deletion failed!"
+         }
+     }
+}
+
+else{
+    echo "Blocked! Not logged in!";
+}
