8eb22a17d06d13d00493d44621ef70867611b0da
Jason <js21kim@gmail.com>
2017-05-20 06:09:08 +0000

    delete

--- docs/friends/delete_friend.php
+++ docs/friends/delete_friend.php
@@ -5,7 +5,7 @@ include ( "../inc/connect.inc.php" );
 
 if ( isset($_SESSION['u_id']) )
 {
-     if( isset($_GET['delete_friend_u_id']) ){
+     if( isset($_GET['delete_friend_u_id']) ) {
          $curr_u_id=$_SESSION['u_id']; // current u_id
          $delete_u_id=$_GET['delete_friend_u_id']; // target id of delete friend
 
@@ -13,7 +13,7 @@ if ( isset($_SESSION['u_id']) )
          $result = mysqli_query($conn, $query);
 
          if ($result) {
-            echo "Successfully deleted   " . $delete_u_id . " from friend list.";
+            echo "Successfully deleted.";
             //echo "<meta http-equiv='refresh' content='0;url=index.php'>";
          }
          else {
