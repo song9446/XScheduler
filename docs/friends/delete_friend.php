fbe7e1411918bb8de27dff0508948479694dc90f
Jason <js21kim@gmail.com>
2017-05-20 06:01:15 +0000

    delete

--- docs/friends/delete_friend.php
+++ docs/friends/delete_friend.php
@@ -13,7 +13,7 @@ if ( isset($_SESSION['u_id']) )
 
          if ($result) {
             echo "Successfully deleted   " . $delete_u_id . " from friend list.";
-            echo "<meta http-equiv='refresh' content='0;url=home.php'>";
+            echo "<meta http-equiv='refresh' content='0;url=index.php'>";
          }
          else {
              echo "Deletion failed!"
