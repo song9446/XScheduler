28a1c28a468c505be9b9c03f974e2ca6454aa75e
Jason <js21kim@gmail.com>
2017-05-31 12:59:14 +0000

    create group

--- docs/group/create_group.php
+++ docs/group/create_group.php
@@ -8,15 +8,17 @@ include ( "../inc/connect.inc.php" );
         $curr_u_id = $_SESSION['u_id'];
         $gn = $_POST['group_name'];
 
-        echo "curr_u_id: $curr_u_id";
-        echo "gn: $gn";
+
 
         $query = "INSERT INTO groups VALUES ('', '$gn', '$curr_u_id')";
         $result = mysqli_query ($conn, $query);
         
-        $g_id = $conn->insert_id;
+        $g_id = $conn->insert_id; // get id of last query. In this case, get the value of auto incrementing g_id.
         echo "g_id: $g_id";
         if ($result) {
+            $query = "INSERT INTO group_member (g_id, u_id) VALUES ('$g_id', '$curr_u_id')";
+            $result = mysqli_query ($conn, $query); 
+
             foreach ($_POST['group_members'] as $member) {
                 $query = "INSERT INTO group_member (g_id, u_id) VALUES ('$g_id', '$member')";
                 $result = mysqli_query ($conn, $query);
