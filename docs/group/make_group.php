9059b55f1d527f65524525ab7b0fbe1d52cb051e
Jason <js21kim@gmail.com>
2017-05-31 11:46:46 +0000

    fix 4

--- docs/group/make_group.php
+++ docs/group/make_group.php
@@ -23,8 +23,8 @@ include ( "../inc/connect.inc.php" );
 
                 echo "<form action='create_group.php' method='post'>";
                 echo "<div class='textbox'>";
-                echo "<label for='group_name'>Group name: </label>";
-                echo "<input type='text' id='group_name' placeholder='Enter group name'>";
+                echo "<h2>Group name: </h2>";
+                echo "<input type='text' name='group_name' placeholder='Enter group name'>";
                 echo "</div>";
 
                 echo "<div class='container'>";
