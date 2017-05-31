8a88f4e82a788faf9e2e901243409953470195b3
Jason <js21kim@gmail.com>
2017-05-31 10:36:04 +0000

    group form 9

--- docs/group/make_group.php
+++ docs/group/make_group.php
@@ -12,28 +12,40 @@ include ( "../inc/connect.inc.php" );
     </head>
 
     <body>
+
+        <?php include ( "../headerMenu.php" ); ?>
+
         <?php
-        
             if ( isset($_SESSION['u_id']) ) {
                 $curr_id = $_SESSION['u_id'];
+
+
+                echo "<form action='#' method='post'>";
+                echo "<div class='textbox'>";
+                echo "<label for='group_name'>Group name: </label>";
+                echo "<input type='text' id='group_name' placeholder='Enter group name'>";
+                echo "</div>";
+
+                echo "<div class='container'>";
 
                 $query = "SELECT f_u_id FROM friend WHERE u_id = '$curr_id'";
                 $result = mysqli_query($conn, $query);
 
                 if ($result) {
-                    echo "<form action='#' method='post'>";
                     echo "<select name='group_members[]' multiple>";
                     while ( $row = mysqli_fetch_assoc($result) ){
                         echo "<option value='" . $row['f_u_id'] . "'>" . $row['f_u_id'] . "</option>";
                     }
                     echo "</select>";
                     echo "<input type='submit' name='submit_group_members' value='get_selected_values' />";
-                    echo "</form>";
                 }
 
                 else {
                     cout << "Error: Failed to get friend data!";
                 }
+
+                echo "</div>";
+                echo "</form>";
 
                 if ( isset($_POST['submit_group_members']) ) {
                     foreach ($_POST['group_members'] as $member)
