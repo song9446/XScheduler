05724d6fff07f223d4eab7b45fc04d342baa9ed6
Jason <js21kim@gmail.com>
2017-06-02 06:52:44 +0000

    16

--- docs/group/index.php
+++ docs/group/index.php
@@ -32,7 +32,12 @@ include ( "../inc/connect.inc.php" );
 
         if ($result){
             while ($row = mysqli_fetch_assoc($result)) {
-                echo "<div class='calendar_main_container'>" . "<a href='index.php?g_id=" . $row['g_id'] . " '>" . "<img class='calendar_main_pic' src='data;base64, " . $row['pic_main'] . " '>" . "</a>" . "<p style='font-size: 10px';>" . row['g_name'] . "</p>" . "<p style='font-size: 10px';>" . row['g_creator'] . </p> . "<p style='font-size: 10px';>" . row['member_num'] . "</p>" . "</div>";
+                echo "<div class='calendar_main_container'>" . 
+                        "<a href='index.php?g_id=" . $row['g_id'] . " '>" . 
+                        "<img class='calendar_main_pic' src='data;base64, " . $row['pic_main'] . " '>" . "</a>" . 
+                        "<p style='font-size: 10px';>" . row['g_name'] . "</p>" . 
+                        "<p style='font-size: 10px';>" . row['g_creator'] . 
+                        "</p>" . "<p style='font-size: 10px';>" . row['member_num'] . "</p>" . "</div>";
             }
         }
         else {
