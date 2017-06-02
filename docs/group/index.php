8ac4f0fb87a5c310f40a6ca53b48c0742f8a9184
Jason <js21kim@gmail.com>
2017-06-02 06:38:53 +0000

    12

--- docs/group/index.php
+++ docs/group/index.php
@@ -11,13 +11,17 @@
 
     <?php
         $curr_u_id = $_SESSION['u_id'];
+
+        echo "$curr_u_id";
 /*
         $query = "SELECT G.g_id, G.g_name, G.g_creator, G.pic_main, MY_G_WITH_COUNT.member_num
                   FROM groups G, (SELECT g_id, COUNT(g_id) AS member_num
                                   FROM group_member 
-                                  WHERE u_id = '$curr_u_id'
+                                  WHERE g_id IN (SELECT g_id 
+                                                 FROM group_member 
+                                                 WHERE u_id = '$curr_u_id') 
                                   GROUP BY g_id) AS MY_G_WITH_COUNT
-                  WHERE G.g_id = MY_G_WITH_COUNT";
+                  WHERE G.g_id = MY_G_WITH_COUNT.g_id";
 
         $result = mysqli_query($conn, $query);
 */
@@ -25,20 +29,19 @@
 
         $result = mysqli_query($conn, $query);
 
-//        if ($result){
-//            while ($row = mysqli_fetch_assoc($result)) {
+        if ($result){
+            while ($row = mysqli_fetch_assoc($result)) {
                 echo $row['g_id'] . "   " . $row['member_num'];
 
 //                echo "<div class='calendar_main_container'>" . "<a href='index.php?g_id=" . $row['g_id'] . " '>" . "<img class='calendar_main_pic' src='data;base64, " . $row['pic_main'] . " '>" . "</a>" . "</div>";
-//            }
+            }
 
 
 
-//        }
-//        else {
-//            echo "Error: could not get my group data.";
-//        }
-
+        }
+        else {
+            echo "Error: could not get my group data.";
+        }
 
 
     ?>
