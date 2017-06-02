b8459fd0f6d44bba80c7dfa7c40fc163b06d8da2
Jason <js21kim@gmail.com>
2017-06-02 05:54:06 +0000

    group/index.php 5

--- docs/group/index.php
+++ docs/group/index.php
@@ -11,8 +11,8 @@
 
     <?php
         $curr_u_id = $_SESSION['u_id'];
-        $query = "SELECT G.g_id, G.g_name, G.g_creator, G.pic_main, MY_G_WITH_COUNT.COUNT
-                  FROM groups G, (SELECT g_id, COUNT(g_id)
+        $query = "SELECT G.g_id, G.g_name, G.g_creator, G.pic_main, MY_G_WITH_COUNT.member_num
+                  FROM groups G, (SELECT g_id, COUNT(g_id) AS member_num
                                   FROM group_member 
                                   WHERE u_id = $curr_u_id 
                                   GROUP BY g_id) AS MY_G_WITH_COUNT
