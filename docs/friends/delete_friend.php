2014c738a7620026a8dec319f002dd59815e3af5
dongju <dju1006@unist.ac.kr>
2017-06-04 06:17:44 +0000

    dj

--- docs/friends/delete_friend.php
+++ docs/friends/delete_friend.php
@@ -10,10 +10,10 @@ if ( isset($_SESSION['u_id']) )
          $delete_u_id=$_GET['delete_f_u_id']; // target id of delete friend
 
          $query1 = "DELETE FROM friend WHERE u_id = '$curr_u_id' AND f_u_id = '$delete_u_id'";
-         $result1 = mysqli_query($conn, $query);
+         $result1 = mysqli_query($conn, $query1);
 
          $query2 = "DELETE FROM friend WHERE u_id = '$delete_u_id' AND f_u_id = '$curr_u_id'";
-         $result2 = mysqli_query($conn, $query);
+         $result2 = mysqli_query($conn, $query2);
 
          if ($result1 && $result2) {
             echo "Successfully deleted.";
