357decf164d936942b60f502a43fe610c6b958e7
Jason <js21kim@gmail.com>
2017-06-04 05:41:07 +0000

    main pic

--- docs/home.php
+++ docs/home.php
@@ -29,7 +29,11 @@ else {
                     $result = mysqli_query($conn, $query);
                     if ($result) {
                         while ($row = mysqli_fetch_assoc($result)) {
-                            echo "<div class='calendar_main_container'>" . "<a href='/scheduler/index.php?g_id=" . $row['g_id'] . " '>" . "<img class='calendar_main_pic' src='data;base64, " . $row['pic_main'] . " '>" . "</a>" . "<p style='font-size: 16px; margin-top: 10px;' align='center';>" . $row['g_name'] . "</p>" . "</div>";
+                            echo "<div class='calendar_main_container'>" . 
+                                    "<a href='/scheduler/index.php?g_id=" . $row['g_id'] . " '>" . 
+                                    "<img class='calendar_main_pic' src='data:image;base64, " . $row['pic_main'] . " '>" . "</a>" . 
+                                    "<p style='font-size: 16px; margin-top: 10px;' align='center';>" . $row['g_name'] . "</p>" . 
+                                 "</div>";
                         }
                     }
 
