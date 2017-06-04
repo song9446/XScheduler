f75e8d00176ddf6de7abc661199d72149731f8e3
Jason <js21kim@gmail.com>
2017-06-04 05:53:38 +0000

    headerMenu fix

--- docs/headerMenu.php
+++ docs/headerMenu.php
@@ -1,24 +1,30 @@
 <!DOCTYPE html>
-    <div class="headerMenu">
-        <!-- <button type="button" id="sidebar_button" onclick="sidebar_toggle('sidebar_button')" value="0">SBB</button> -->
-        <div class="logo">
-            <a href="http://52.78.81.68/home.php" class="logo">
-                <img src="http://52.78.81.68/img/logo.png" width="42" height="42"/>
-            </a>
-        </div>
-        <nav id="topMenu">
-          <ul>
-              <li>
-                <a class="menuLink" href="http://52.78.81.68/scheduler/index.php">Personal schedule</a>
-              </li>
-              <li>
-                <a class="menuLink" href="http://52.78.81.68/group/index.php">Group schedule</a>
-              </li>
-              <li>
-                <a class="menuLink" href="http://52.78.81.68/friends/index.php">Friends</a>
-              </li>
-          </ul>
-        </nav>
+<html>
+    <head>
+        <title>UniScheduler</title>
+        <link rel="stylesheet" type="text/css" href="http://52.78.81.68/css/style.css" />
+    </head>
+
+    <body>
+        <div class="headerMenu">
+            <div class="logo">
+                <a href="http://52.78.81.68/home.php" class="logo">
+                    <img src="http://52.78.81.68/img/logo.png" width="42" height="42"/>
+                </a>
+            </div>
+            <nav id="topMenu">
+                <ul>
+                    <li>
+                        <a class="menuLink" href="http://52.78.81.68/scheduler/index.php">Personal schedule</a>
+                    </li>
+                    <li>
+                        <a class="menuLink" href="http://52.78.81.68/group/index.php">Group schedule</a>
+                    </li>
+                    <li>
+                        <a class="menuLink" href="http://52.78.81.68/friends/index.php">Friends</a>
+                    </li>
+                 </ul>
+            </nav>
             <?php
                 if (isset($_SESSION['u_id'])){
                     echo "<a href='http://52.78.81.68/logout.php' class='logout_button'>Logout</a>";
@@ -28,6 +34,6 @@
                     include ( "./register.php" );
                 }
             ?>
-    </div>
-
-    <script src="./js/script1.js"></script>
+        </div>
+    </body>
+</html>
